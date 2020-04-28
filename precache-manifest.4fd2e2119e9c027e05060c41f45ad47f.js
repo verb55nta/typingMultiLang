997dbdc8f8b1648bafaefb7f6c9f336ced8c03fb
verb55 <verb.storage.in.net@gmail.com>
2020-04-28 19:12:56 +0900

    Updates

---
+++ precache-manifest.4fd2e2119e9c027e05060c41f45ad47f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59cef2be3d61f36fdf227ceb5e7bc876",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf3ed9e114fab453b0e8",
+    "url": "/static/css/main.099a6a36.chunk.css"
+  },
+  {
+    "revision": "405f802a4162de622db6",
+    "url": "/static/js/2.1aa310c0.chunk.js"
+  },
+  {
+    "revision": "066a3b5f5d30e9efcd56434ebe4db621",
+    "url": "/static/js/2.1aa310c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf3ed9e114fab453b0e8",
+    "url": "/static/js/main.f13c633b.chunk.js"
+  },
+  {
+    "revision": "7424bb2dd81bb1124b20",
+    "url": "/static/js/runtime-main.6e238f4c.js"
+  }
+]);
